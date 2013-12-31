190eed04f6d80a0ec6b2c773fb6332f7cb3fdd2a
Mark Hanna <mark.hanna@gmail.com>
2013-12-31 14:22:04 -0500

    implemented devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinteresting::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" # GET creates 'about' path. if we said GET something, then we'd have to say something_path
 
